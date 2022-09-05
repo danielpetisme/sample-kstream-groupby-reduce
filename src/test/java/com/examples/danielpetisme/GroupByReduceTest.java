7967c7b319054d30494e398aaf58817f37703737
Daniel Petisme <daniel.petisme@gmail.com>
2022-09-05 10:22:41 +0200

    Initial Commit

--- src/test/java/com/examples/danielpetisme/GroupByReduceTest.java
+++ src/test/java/com/examples/danielpetisme/GroupByReduceTest.java
@@ -17,7 +17,8 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.apache.kafka.streams.*;
 import org.apache.kafka.streams.kstream.*;
 import org.apache.kafka.streams.test.TestRecord;
-import org.junit.Test;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.KafkaContainer;
@@ -42,6 +43,7 @@ public class GroupByReduceTest {
     static final String IputTopic1 = "in1";
     static final String OutputTopic = "out";
 
+
     Topology createTopologyWithNoSuppress() {
 
         final var builder = new StreamsBuilder();
@@ -53,6 +55,24 @@ public class GroupByReduceTest {
                 .reduce((v1, v2) -> v1 + v2)
                 .toStream()
                 .map((Windowed<String> key, String value) -> new KeyValue<>(key.key(), value))
+                .peek((k, v) -> LOGGER.info("Produced values for key [" + k + "] => [" + v + "]"))
+                .to(OutputTopic,
+                        Produced.<String, String>as("aggregated-value")
+                                .withKeySerde(Serdes.String()).withValueSerde(Serdes.String()));
+        return builder.build();
+    }
+
+    Topology createTopologyWithNoSuppressAndWindows() {
+
+        final var builder = new StreamsBuilder();
+
+        builder.stream(IputTopic1, Consumed.with(Serdes.String(), Serdes.String()))
+                .peek((k, v) -> LOGGER.info("input values for key [" + k + "] => [" + v + "]"))
+                .groupByKey()
+                .windowedBy(SessionWindows.ofInactivityGapAndGrace(Duration.ofSeconds(5), Duration.ofSeconds(1)))
+                .reduce((v1, v2) -> v1 + v2)
+                .toStream()
+                .map((Windowed<String> key, String value) -> new KeyValue<>(key.key() + "-" + key.window(), value))
                 .peek((k, v) -> LOGGER.info("Produced values for key [" + k + "] => [" + v + "]"))
                 .to(OutputTopic,
                         Produced.<String, String>as("aggregated-value")
@@ -99,7 +119,7 @@ public class GroupByReduceTest {
 //        Producer values for key [1] => [abc]
 //        Input values for key [1] => [a]
 //        Producer values for key [1] => [a]
-    final List<TestRecord<String, String>> expectedWithoutSuppress = List.of(
+    final List<TestRecord<String, String>> expectedWithNoCacheAndNoSuppress = List.of(
             new TestRecord("1", "a", null, 1L),
             new TestRecord("1", null, null, 1L),
             new TestRecord("1", "ab", null, 2L),
@@ -118,26 +138,30 @@ public class GroupByReduceTest {
     //https://kafka.apache.org/32/javadoc/org/apache/kafka/streams/TopologyTestDriver.html
     // Note that the TopologyTestDriver processes input records synchronously. This implies that commit.interval.ms and cache.max.bytes.buffering configuration have no effect. The driver behaves as if both configs would be set to zero, i.e., as if a "commit" (and thus "flush") would happen after each input record.
     @Test
+    @Order(1)
     public void runTestWithTopologyTestDriver_And_WithNoSuppress() throws Exception {
-        runTopologyTestDriver(createTopologyWithNoSuppress(), input, expectedWithoutSuppress);
+        runTopologyTestDriver(createTopologyWithNoSuppress(), input, expectedWithNoCacheAndNoSuppress);
     }
 
 
     // This test fail on purpose
     @Test
+    @Order(2)
     public void runTestWithKafka_And_WithCache_And_WithNoSuppress() throws Exception {
+
         try (KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka"))) {
             kafka.start();
             runTestContainer(
                     kafka,
                     createTopologyWithNoSuppress(),
-                    input, expectedWithoutSuppress,
+                    input, expectedWithNoCacheAndNoSuppress,
                     Collections.emptyMap()
             );
         }
     }
 
     @Test
+    @Order(3)
     public void runTestWithKafka_And_WithoutKafkaStreamsCache_And_WithNoSuppress() throws Exception {
         try (KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka"))) {
             kafka.start();
@@ -145,19 +169,75 @@ public class GroupByReduceTest {
                     kafka,
                     createTopologyWithNoSuppress(),
                     input,
-                    expectedWithoutSuppress,
+                    expectedWithNoCacheAndNoSuppress,
                     Map.of(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, "0")
             );
         }
     }
 
+    @Test
+    @Order(4)
+    public void runTestWithKafka_And_WithCache_And_WithNoSuppress_Demonstration() throws Exception {
+        final List<TestRecord<String, String>> expectedWithCacheAndNoSuppressDefaultConfig = List.of(
+//                new TestRecord("1", "a", null, 1L), These intermediate events are cached
+//                new TestRecord("1", null, null, 1L),
+//                new TestRecord("1", "ab", null, 2L),
+                new TestRecord("2", "a", null, 3L),
+//                new TestRecord("1", null, null, 2L),
+                new TestRecord("1", "abc", null, 4L),
+                new TestRecord("1", "a", null, 25_000L)
+        );
+
+        //When cache is not tweak, the changes are masked by Kafka Streams cache and only the final result is propagated
+        try (KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka"))) {
+            kafka.start();
+            runTestContainer(
+                    kafka,
+                    createTopologyWithNoSuppress(),
+                    input, expectedWithCacheAndNoSuppressDefaultConfig,
+                    Collections.emptyMap()
+            );
+        }
+
+        final List<TestRecord<String, String>> expectedWithNoCacheAndNoSuppressCachingCommitIntervalUpdated = List.of(
+//                new TestRecord("1", "a", null, 1L), These intermediate events are cached
+//                new TestRecord("1", null, null, 1L),
+                new TestRecord("1", "ab", null, 2L),
+                new TestRecord("2", "a", null, 3L),
+                new TestRecord("1", null, null, 2L),
+                new TestRecord("1", "abc", null, 4L),
+                new TestRecord("1", "a", null, 25_000L)
+        );
+
+        //The cache size may alter the generated events
+        try (KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka"))) {
+            kafka.start();
+            runTestContainer(
+                    kafka,
+                    createTopologyWithNoSuppress(),
+                    input, expectedWithNoCacheAndNoSuppressCachingCommitIntervalUpdated,
+                    Map.of(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, "100", StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, "100")
+            );
+        }
+
+        //Default config producer less intermediate event than a bit of caching
+        assertThat(expectedWithCacheAndNoSuppressDefaultConfig.size()).isLessThan(expectedWithNoCacheAndNoSuppressCachingCommitIntervalUpdated.size());
+
+        //A bit of caching producer less events than no caching
+        assertThat(expectedWithNoCacheAndNoSuppressCachingCommitIntervalUpdated.size()).isLessThan(expectedWithNoCacheAndNoSuppress.size());
+
+    }
+
+
     // Suppress filter intermediate result per key and window
     @Test
+    @Order(5)
     public void runTestWithTopologyTestDriver_And_WithSuppress() throws Exception {
         runTopologyTestDriver(createTopologyWithSuppress(), input, expectedWithSuppress);
     }
 
     @Test
+    @Order(6)
     public void runTestWithKafka_And_WithCache_And_WithSuppress() throws Exception {
         try (KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka"))) {
             kafka.start();
@@ -171,6 +251,7 @@ public class GroupByReduceTest {
     }
 
     @Test
+    @Order(7)
     public void runTestWithKafka_And_WithoutKafkaStreamsCache_And_WithSuppress() throws Exception {
         try (KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka"))) {
             kafka.start();
@@ -183,10 +264,10 @@ public class GroupByReduceTest {
         }
     }
 
-
     // Suppress is triggering at processing time and requires closing events (ie. events occuring after the window period)
     @Test
-    public void runTestWithTopologyTestDriver_And_WithSuppress_And_Closing_Event() throws Exception {
+    @Order(8)
+    public void runTestWithTopologyTestDriver_And_WithSuppress_And_ClosingEvent() throws Exception {
         runTopologyTestDriver(createTopologyWithSuppress(), input, expectedWithSuppress);
 
         var inputWithClosingEvent = new ArrayList<>(input);
@@ -195,6 +276,111 @@ public class GroupByReduceTest {
         var expectedWithClosingEvent = new ArrayList<>(expectedWithSuppress);
         expectedWithClosingEvent.add(new TestRecord<>("1", "a", null, 25_000L));
         runTopologyTestDriver(createTopologyWithSuppress(), inputWithClosingEvent, expectedWithClosingEvent);
+    }
+
+    @Test
+    @Order(9)
+    // Counting he windows open // closed
+    public void runTestWithTopologyTestDriver_And_WithNoSuppress_And_ClosingEvent() throws Exception {
+
+        // When events are generated within the session window, the window extends
+        List<TestRecord<String, String>> inputWithEventInSessionWindow = List.of(
+                new TestRecord("1", "a", null, 1L),
+                new TestRecord("1", "b", null, 2L),
+                new TestRecord("2", "a", null, 3L),
+                new TestRecord("1", "c", null, 4L),
+                new TestRecord("1", "d", null, 5_000L), //<--Adding an event in the session window
+                new TestRecord("1", "a", null, 25_000L)
+        );
+
+        List<TestRecord<String, String>> expectedWithEventInSessionWindow = List.of(
+                new TestRecord("1-Window{startMs=1, endMs=1}", "a", null, 1L),
+                new TestRecord("1-Window{startMs=1, endMs=1}", null, null, 1L),
+                new TestRecord("1-Window{startMs=1, endMs=2}", "ab", null, 2L),
+                new TestRecord("2-Window{startMs=3, endMs=3}", "a", null, 3L),
+                new TestRecord("1-Window{startMs=1, endMs=2}", null, null, 2L),
+                new TestRecord("1-Window{startMs=1, endMs=4}", "abc", null, 4L),
+                new TestRecord("1-Window{startMs=1, endMs=4}", null, null, 4L),
+                new TestRecord("1-Window{startMs=1, endMs=5000}", "abcd", null, 5_000L),
+                new TestRecord("1-Window{startMs=25000, endMs=25000}", "a", null, 25_000L)
+        );
+
+        runTopologyTestDriver(createTopologyWithNoSuppressAndWindows(), inputWithEventInSessionWindow, expectedWithEventInSessionWindow);
+        try (KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka"))) {
+            kafka.start();
+            runTestContainer(
+                    kafka,
+                    createTopologyWithNoSuppressAndWindows(),
+                    inputWithEventInSessionWindow, expectedWithEventInSessionWindow,
+                    Map.of(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, "0")
+            );
+        }
+
+        List<TestRecord<String, String>> expectedWithEventInSessionWindowWithCache = List.of(
+                new TestRecord("2-Window{startMs=3, endMs=3}", "a", null, 3L),
+                new TestRecord("1-Window{startMs=1, endMs=5000}", "abcd", null, 5_000L),
+                new TestRecord("1-Window{startMs=25000, endMs=25000}", "a", null, 25_000L)
+        );
+
+        try (KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka"))) {
+            kafka.start();
+            runTestContainer(
+                    kafka,
+                    createTopologyWithNoSuppressAndWindows(),
+                    inputWithEventInSessionWindow, expectedWithEventInSessionWindowWithCache,
+                    Collections.emptyMap()
+            );
+        }
+
+        // When events are generated outside the session window, a new window is created
+        List<TestRecord<String, String>> inputWithEventOutSessionWindow = List.of(
+                new TestRecord("1", "a", null, 1L),
+                new TestRecord("1", "b", null, 2L),
+                new TestRecord("2", "a", null, 3L),
+                new TestRecord("1", "c", null, 4L),
+                new TestRecord("1", "d", null, 7_000L), //<--Adding an event outside the session window
+                new TestRecord("1", "a", null, 25_000L)
+        );
+
+        List<TestRecord<String, String>> expectedWithEventOutSessionWindow = List.of(
+                new TestRecord("1-Window{startMs=1, endMs=1}", "a", null, 1L),
+                new TestRecord("1-Window{startMs=1, endMs=1}", null, null, 1L),
+                new TestRecord("1-Window{startMs=1, endMs=2}", "ab", null, 2L),
+                new TestRecord("2-Window{startMs=3, endMs=3}", "a", null, 3L),
+                new TestRecord("1-Window{startMs=1, endMs=2}", null, null, 2L),
+                new TestRecord("1-Window{startMs=1, endMs=4}", "abc", null, 4L),
+                new TestRecord("1-Window{startMs=7000, endMs=7000}", "d", null, 7_000L),
+                new TestRecord("1-Window{startMs=25000, endMs=25000}", "a", null, 25_000L)
+        );
+
+        runTopologyTestDriver(createTopologyWithNoSuppressAndWindows(), inputWithEventOutSessionWindow, expectedWithEventOutSessionWindow);
+        try (KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka"))) {
+            kafka.start();
+            runTestContainer(
+                    kafka,
+                    createTopologyWithNoSuppressAndWindows(),
+                    inputWithEventOutSessionWindow, expectedWithEventOutSessionWindow,
+                    Map.of(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, "0")
+            );
+        }
+
+        List<TestRecord<String, String>> expectedWithEventOutSessionWindowWithCache = List.of(
+                new TestRecord("2-Window{startMs=3, endMs=3}", "a", null, 3L),
+                new TestRecord("1-Window{startMs=1, endMs=4}", "abc", null, 4L),
+                new TestRecord("1-Window{startMs=7000, endMs=7000}", "d", null, 7_000L),
+                new TestRecord("1-Window{startMs=25000, endMs=25000}", "a", null, 25_000L)
+
+        );
+
+        try (KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka"))) {
+            kafka.start();
+            runTestContainer(
+                    kafka,
+                    createTopologyWithNoSuppressAndWindows(),
+                    inputWithEventOutSessionWindow, expectedWithEventOutSessionWindowWithCache,
+                    Collections.emptyMap()
+            );
+        }
     }
 
 
@@ -290,7 +476,7 @@ public class GroupByReduceTest {
         streams.start();
         Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
 
-        this.input.forEach((record) -> {
+        inputRecords.forEach((record) -> {
             try {
                 inputProducer.send(
                         new ProducerRecord<>(IputTopic1, 0, record.timestamp(), record.key(), record.value()),
@@ -311,8 +497,8 @@ public class GroupByReduceTest {
         List<ConsumerRecord<String, String>> loaded = new ArrayList<>();
 
         long start = System.currentTimeMillis();
-        while (loaded.isEmpty() && System.currentTimeMillis() - start < 40_000) {
-            ConsumerRecords<String, String> records = outputConsumer.poll(Duration.of(10, ChronoUnit.SECONDS));
+        while (loaded.size() < expectedRecords.size() && System.currentTimeMillis() - start < 40_000) {
+            ConsumerRecords<String, String> records = outputConsumer.poll(Duration.of(3, ChronoUnit.SECONDS));
             records.forEach((record) -> loaded.add(record));
         }
 
@@ -325,6 +511,4 @@ public class GroupByReduceTest {
                 .collect(Collectors.toList());
         assertThat(actualRecords).hasSameElementsAs(expectedRecords);
     }
-
-
 }
